5f122910b67602f81dd70877e3c9b1af08919957
sofiaversoza <sofversoza@gmail.com>
2022-01-20 00:33:04 -0700

    commit tree 29af5d9c51844646f98851774efba7910a9bbd9a

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,8 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  
+
   attr_accessor :name, :grade
-  
   attr_reader :id
   
   def initialize(name, grade, id=nil)
@@ -12,7 +11,7 @@ class Student
     @name = name
     @grade = grade
   end
-  
+
   def self.create_table
     sql =  <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
